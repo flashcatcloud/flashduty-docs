705c3ba69ef70eb3928f2dc7bf89604cd3e51d71
pijiang <419471640@qq.com>
2026-09-21 11:06:34 +0800

    docs: add Checkly alert integration

--- integration-docs/src/doc-map.mjs
+++ integration-docs/src/doc-map.mjs
@@ -66,6 +66,7 @@ export const docMap = {
   Datadog: `${alertBase}/datadog.mdx`,
   NewRelic: `${alertBase}/new-relic.mdx`,
   SumoLogic: `${alertBase}/sumo-logic.mdx`,
+  Checkly: `${alertBase}/checkly.mdx`,
   AliyunPrometheus: `${alertBase}/aliyun-prometheus.mdx`,
   AliyunDataWorksOp: `${alertBase}/aliyun-dataworks-op.mdx`,
   HttpPull: `${alertBase}/http-pull.mdx`,
