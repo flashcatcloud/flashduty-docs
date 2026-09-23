a877017a983bf1dc13d8c07cc6ff008ddc56f658
ysyneu <ysyneu@gmail.com>
2026-09-23 06:21:49 -0700

    docs(alert-sources): add PRTG integration guide

--- integration-docs/src/doc-map.mjs
+++ integration-docs/src/doc-map.mjs
@@ -74,6 +74,7 @@ export const docMap = {
   StatusCake: `${alertBase}/statuscake.mdx`,
   Elastic: `${alertBase}/elastic.mdx`,
   Checkly: `${alertBase}/checkly.mdx`,
+  PRTG: `${alertBase}/prtg.mdx`,
   AliyunPrometheus: `${alertBase}/aliyun-prometheus.mdx`,
   AliyunDataWorksOp: `${alertBase}/aliyun-dataworks-op.mdx`,
   HttpPull: `${alertBase}/http-pull.mdx`,
