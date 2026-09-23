7230bf6192b2c2df35a04a7cc459833b4a534396
ysyneu <ysyneu@gmail.com>
2026-09-23 00:13:09 -0700

    docs(on-call): add StatusCake alert integration guide

--- integration-docs/src/doc-map.mjs
+++ integration-docs/src/doc-map.mjs
@@ -66,6 +66,7 @@ export const docMap = {
   Datadog: `${alertBase}/datadog.mdx`,
   NewRelic: `${alertBase}/new-relic.mdx`,
   SumoLogic: `${alertBase}/sumo-logic.mdx`,
+  StatusCake: `${alertBase}/statuscake.mdx`,
   AliyunPrometheus: `${alertBase}/aliyun-prometheus.mdx`,
   AliyunDataWorksOp: `${alertBase}/aliyun-dataworks-op.mdx`,
   HttpPull: `${alertBase}/http-pull.mdx`,
