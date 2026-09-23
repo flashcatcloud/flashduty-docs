d47c526a765698f3694738eb5823bc5f91aa0def
ysyneu <ysyneu@gmail.com>
2026-09-23 00:12:15 -0700

    docs(integration): add Rollbar alert integration guide

--- integration-docs/src/doc-map.mjs
+++ integration-docs/src/doc-map.mjs
@@ -66,6 +66,7 @@ export const docMap = {
   Datadog: `${alertBase}/datadog.mdx`,
   NewRelic: `${alertBase}/new-relic.mdx`,
   SumoLogic: `${alertBase}/sumo-logic.mdx`,
+  Rollbar: `${alertBase}/rollbar.mdx`,
   AliyunPrometheus: `${alertBase}/aliyun-prometheus.mdx`,
   AliyunDataWorksOp: `${alertBase}/aliyun-dataworks-op.mdx`,
   HttpPull: `${alertBase}/http-pull.mdx`,
